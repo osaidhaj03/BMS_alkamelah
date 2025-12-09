600be95ab9109dc95ba48d86d1df9021a7436c21
osaidhaj <osaidhaj03@gmail.com>
2025-12-09 23:59:00 +0300

    feat: Refactor main layout to streamline meta tags and improve performance

--- resources/views/components/superduper/main-backup.blade.php
+++ resources/views/components/superduper/main-backup.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     
+    <title>{{ $generalSettings->brand_name ?? config('app.name', 'BMS Alkamelah') }}</title>
     
     <!-- Favicon -->
     <link rel="shortcut icon" href="{{ asset('superduper/img/favicon.png') }}" type="image/x-icon">
